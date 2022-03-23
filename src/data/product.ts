b27042194ab8b1767ae26ab7cb480f7da73b5ef8
dymafr <erwan1309@hotmail.com>
2022-03-23 10:57:37 +0100

    Initial commit

---
+++ src/data/product.ts
@@ -0,0 +1,84 @@
+import type { ProductInterface } from './interfaces';
+
+export default [
+  {
+    id: 1,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/macbookpro.PNG',
+    title: 'Macbook Pro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1500,
+    category: 'desktop',
+  },
+  {
+    id: 2,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/levono.PNG',
+    title: 'Levono Pro',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 2300,
+    category: 'desktop',
+  },
+  {
+    id: 3,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/rider.PNG',
+    title: 'Rider',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1200,
+    category: 'desktop',
+  },
+  {
+    id: 4,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/ldlc.PNG',
+    title: 'LDLC benolo',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 4500,
+    category: 'gamer',
+  },
+  {
+    id: 5,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/asus.PNG',
+    title: 'Asus gamer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 3755,
+    category: 'streaming',
+  },
+  {
+    id: 6,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/rog.PNG',
+    title: 'Rog desktop',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 2452,
+    category: 'streaming',
+  },
+  {
+    id: 7,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/msi.PNG',
+    title: 'MSI play',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 1478,
+    category: 'gaming',
+  },
+  {
+    id: 8,
+    image:
+      'https://stackblitz.com/files/vitejs-vite-auyhqv/github/dymafr/vue-c8l8/master/src/assets/images/pad.PNG',
+    title: 'Think pad',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing edolor tempore ipsam cum ipsum reiciendis',
+    price: 899,
+    category: 'desktop',
+  },
+] as ProductInterface[];
---
+++ src/interfaces/Product.interface.ts
@@ -0,0 +1,10 @@
+import type { Category } from './type';
+
+export interface ProductInterface {
+  id: number;
+  title: string;
+  image: string;
+  price: number;
+  description: string;
+  category: Category;
+}
